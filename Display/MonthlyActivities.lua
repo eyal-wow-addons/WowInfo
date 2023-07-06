178b3c0cd5fb12febf1714cd02ae9d9f0dfc80cf
Eyal Alon <eyalon81@gmail.com>
2023-07-06 13:59:54 +0300

    Fix an error that appears when item reward data is not loaded

--- Display/MonthlyActivities.lua
+++ Display/MonthlyActivities.lua
@@ -3,6 +3,7 @@ local Display = addon:NewDisplay("MonthlyActivities")
 local EncounterJournal = addon.EncounterJournal
 
 local MONTHLY_ACTIVITIES_LABEL = MONTHLY_ACTIVITIES_TAB .. ":"
+local MONTHLY_ACTIVITIES_PROGRESSED = MONTHLY_ACTIVITIES_PROGRESSED .. ":"
 
 local itemTextureSettings = {
     width = 14,
@@ -11,20 +12,26 @@ local itemTextureSettings = {
     margin = { right = 2, top = 2, bottom = 2 },
 }
 
-Display:RegisterHookScript(EJMicroButton, "OnEnter", function()
+Display:RegisterHookScript(EJMicroButton, "OnEnter", function(self)
     local thresholdProgressString, itemReward, pendingReward, monthString, timeString = EncounterJournal:GetMonthlyActivitiesInfo()
 
     if thresholdProgressString then
+        if itemReward and not itemReward:IsItemDataCached() then
+            self.itemDataLoadedCancelFunc = itemReward:ContinueOnItemLoad(GenerateClosure(self.OnEnter, self))
+        end
+    
+        self.itemDataLoadedCancelFunc = nil
+        
         Display:AddEmptyLine()
-        Display:AddHighlightLine(MONTHLY_ACTIVITIES_LABEL)
+        Display:AddHighlightDoubleLine(MONTHLY_ACTIVITIES_LABEL, monthString)
         Display:AddEmptyLine()
-        Display:AddHighlightDoubleLine(monthString, timeString)
+        Display:AddHighlightDoubleLine(" ", timeString)
         Display:AddEmptyLine()
 
         if itemReward then
             local itemName = itemReward:GetItemName()
-            local itemColor = itemReward:GetItemQualityColor()
-            Display:AddLine(MONTHLY_ACTIVITIES_PROGRESSED)
+            local itemColor = itemReward:GetItemQualityColor() or HIGHLIGHT_FONT_COLOR
+            Display:AddHighlightLine(MONTHLY_ACTIVITIES_PROGRESSED)
             Display:AddRightHighlightDoubleLine(itemName, thresholdProgressString, itemColor.r, itemColor.g, itemColor.b)
             Display:AddTexture(itemReward:GetItemIcon(), itemTextureSettings)
         else
@@ -38,5 +45,13 @@ Display:RegisterHookScript(EJMicroButton, "OnEnter", function()
 
         Display:Show()
     end
+end)
+
+Display:RegisterHookScript(EJMicroButton, "OnLeave", function(self)
+    Display:Hide()
+	if self.itemDataLoadedCancelFunc then
+		self.itemDataLoadedCancelFunc()
+		self.itemDataLoadedCancelFunc = nil
+	end
 end)
 
