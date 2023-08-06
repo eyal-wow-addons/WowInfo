255cdbe67acb9b0719e58eee02195ab71f1f38ce
Eyal Alon <eyalon81@gmail.com>
2023-08-06 06:31:06 +0300

    Improve Tooltip proxy
    1. All functions are now using self as opposed to GameTooltip.
    2. Add UseTooltip function that allows a tooltip object to use the Tooltip's functions, this is useful for LDB plugins.

--- Core/Tooltip.lua
+++ Core/Tooltip.lua
@@ -7,42 +7,59 @@ local EMPTY = " "
 local GameTooltip = GameTooltip
 local HIGHLIGHT_FONT_COLOR = HIGHLIGHT_FONT_COLOR
 
+local Tooltip_Redirect = function(t, k)
+    return Tooltip[k]
+end
+
+local Tooltip_MT = { __index = Tooltip_Redirect }
+
+function Tooltip:UseTooltip(tooltip)
+    if tooltip ~= self.__tooltip then
+        if tooltip == GameTooltip or tooltip == Tooltip then
+            self.__tooltip = self
+        else
+            self.__tooltip = setmetatable(tooltip, Tooltip_MT)
+        end
+    end
+    return self.__tooltip
+end
+
 function Tooltip:AddEmptyLine()
-    GameTooltip:AddLine(EMPTY)
+    self:AddLine(EMPTY)
 end
 
 function Tooltip:AddIndentedLine(text, ...)
-    GameTooltip:AddLine("  " .. text, ...)
+    self:AddLine("  " .. text, ...)
 end
 
 function Tooltip:AddHighlightLine(text)
-    GameTooltip:AddLine(text, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
+    self:AddLine(text, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
 end
 
 function Tooltip:AddGrayLine(text)
-    GameTooltip:AddLine(text, GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b)
+    self:AddLine(text, GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b)
 end
 
 function Tooltip:AddIndentedDoubleLine(textLeft, ...)
-    GameTooltip:AddDoubleLine("  " .. textLeft, ...)
+    self:AddDoubleLine("  " .. textLeft, ...)
 end
 
 function Tooltip:AddLeftHighlightDoubleLine(textLeft, textRight, r, g, b)
-    GameTooltip:AddDoubleLine(textLeft, textRight, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b, r, g, b)
+    self:AddDoubleLine(textLeft, textRight, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b, r, g, b)
 end
 
 function Tooltip:AddRightHighlightDoubleLine(textLeft, textRight, r, g, b)
-    GameTooltip:AddDoubleLine(textLeft, textRight, r, g, b, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
+    self:AddDoubleLine(textLeft, textRight, r, g, b, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
 end
 
 function Tooltip:AddHighlightDoubleLine(textLeft, textRight)
-    GameTooltip:AddDoubleLine(textLeft, textRight, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
+    self:AddDoubleLine(textLeft, textRight, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
 end
 
 function Tooltip:AddGrayDoubleLine(textLeft, textRight)
-    GameTooltip:AddDoubleLine(textLeft, textRight, GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b, GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b)
+    self:AddDoubleLine(textLeft, textRight, GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b, GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b)
 end
 
 function Tooltip:AddLeftGrayDoubleLine(textLeft, textRight)
-    GameTooltip:AddDoubleLine(textLeft, textRight, nil, nil, nil, GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b)
+    self:AddDoubleLine(textLeft, textRight, nil, nil, nil, GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b)
 end
