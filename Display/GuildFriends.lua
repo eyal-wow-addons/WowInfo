82fe5255147e308ae576248297edb247f75d8412
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-01-17 04:46:26 +0200

    Add label that indicates the amount of (online) guild members

--- Display/GuildFriends.lua
+++ Display/GuildFriends.lua
@@ -2,20 +2,26 @@ local _, addon = ...
 local Display = addon:NewDisplay("GuildFriends")
 local Guild = addon.Guild
 
+local GUILD_MEMBERS_LABEL_FORMAT = "|cff20ff20%d|r Members |cff20ff20%d|r Online"
 local GUILD_TOTAL_FRIENDS_LABEL_FORMAT = "Guild Friends (%d):"
 
 Display:RegisterHookScript(GuildMicroButton, "OnEnter", function()
-    local totalOnlineGuildFriends = Guild:GetTotalOnlineFriends()
-    if totalOnlineGuildFriends > 0 then
+    local numTotalGuildMembers, numOnlineGuildMembers = Guild:GetNumGuildMembers()
+    if numTotalGuildMembers > 0 then
         Display:AddEmptyLine()
-        Display:AddHighlightLine(GUILD_TOTAL_FRIENDS_LABEL_FORMAT:format(totalOnlineGuildFriends))
-        for nameString, zoneString in Guild:IterableOnlineFriendsInfo() do
-            if zoneString then
-                Display:AddDoubleLine(nameString, zoneString)
-            else
-                Display:AddDoubleLine(nameString, zoneString, GRAY_FONT_COLOR:GetRGB())
-            end
-        end 
+        Display:AddHighlightLine(GUILD_MEMBERS_LABEL_FORMAT:format(numTotalGuildMembers, numOnlineGuildMembers))
+        local totalOnlineGuildFriends = Guild:GetTotalOnlineFriends()
+        if totalOnlineGuildFriends > 0 then
+            Display:AddEmptyLine()
+            Display:AddHighlightLine(GUILD_TOTAL_FRIENDS_LABEL_FORMAT:format(totalOnlineGuildFriends))
+            for nameString, zoneString in Guild:IterableOnlineFriendsInfo() do
+                if zoneString then
+                    Display:AddDoubleLine(nameString, zoneString)
+                else
+                    Display:AddDoubleLine(nameString, zoneString, GRAY_FONT_COLOR:GetRGB())
+                end
+            end 
+        end
         Display:Show()
     end
 end)
