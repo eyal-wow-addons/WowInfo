1cca31c84f2b558687cea4f9ae6ba50d80a42f70
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-02-18 06:59:09 +0200

    Add PvP tier and reward information

--- DataAPI/PvP.lua
+++ DataAPI/PvP.lua
@@ -1,5 +1,6 @@
 local _, addon = ...
 local PvP = addon:NewObject("PvP")
+local Achievements = addon.Achievements
 
 local NO_ARENA_SEASON = 0
 
@@ -58,6 +59,17 @@ function PvP:GetPlayerProgressInfo(isActiveSeason, isOffSeason)
     return HONOR_LEVEL_LABEL:format(honorLevel), STANDING_FORMAT:format(currentHonor, maxHonor)
 end
 
+local function GetSeasonTierInfo(bracketTierID)
+    local tierID, nextTierID = C_PvP.GetSeasonBestInfo()
+    local tierInfo = C_PvP.GetPvpTierInfo(bracketTierID)
+    local nextTierName
+    if tierID == bracketTierID and nextTierID then
+        local nextTierInfo = C_PvP.GetPvpTierInfo(nextTierID)
+        nextTierName = TOOLTIP_PVP_NEXT_RANK:format(PVPUtil.GetTierName(nextTierInfo.pvpTierEnum))
+    end
+    return PVPUtil.GetTierName(tierInfo.pvpTierEnum), tierInfo.tierIconID, nextTierName
+end
+
 function PvP:IterableArenaProgressInfo()
     local i = 0
     local n = #CONQUEST_SIZE_STRINGS
@@ -66,7 +78,7 @@ function PvP:IterableArenaProgressInfo()
         if i <= n then
             local name = CONQUEST_SIZE_STRINGS[i]
             local bracketIndex = CONQUEST_BRACKET_INDEXES[i]
-            local rating, _, _, _, _, weeklyPlayed, weeklyWon = GetPersonalRatedInfo(bracketIndex)
+            local rating, _, _, _, _, weeklyPlayed, weeklyWon,_, _, pvpTier = GetPersonalRatedInfo(bracketIndex)
             local ratingString, weeklyStatusString
             if rating > 0 then
                 ratingString = RATED_PVP_LABEL_FORMAT:format(name, rating)
@@ -75,8 +87,42 @@ function PvP:IterableArenaProgressInfo()
                 ratingString = GRAY_FONT_COLOR:WrapTextInColorCode(name)
                 weeklyStatusString = GRAY_FONT_COLOR:WrapTextInColorCode(0)
             end
-            return ratingString, weeklyStatusString
+            return ratingString, weeklyStatusString, GetSeasonTierInfo(pvpTier)
         end
     end
 end
 
+function PvP:GetSeasonRewardInfo()
+	local achievementID = C_PvP.GetPVPSeasonRewardAchievementID()
+
+	if achievementID then
+		while true do
+			local completed = select(4, GetAchievementInfo(achievementID))
+			if not completed then
+				break
+			end
+
+			local supercedingAchievements = C_AchievementInfo.GetSupercedingAchievements(achievementID)
+			if not supercedingAchievements[1] then
+				break
+			end
+
+			achievementID = supercedingAchievements[1]
+		end
+	end
+
+    if not achievementID or GetAchievementNumCriteria(achievementID) == 0 then
+		return
+	end
+
+    local criteriaString, _, completed, quantity, reqQuantity = GetAchievementCriteriaInfo(achievementID, 1)
+
+    if criteriaString and not completed then
+        local rewardItemID = C_AchievementInfo.GetRewardItemID(achievementID)
+        if rewardItemID then
+            local itemName, _, quality, _, _, _, _, _, _, itemTexture = GetItemInfo(rewardItemID)
+            local itemQualityColor = quality and BAG_ITEM_QUALITY_COLORS[quality] or HIGHLIGHT_FONT_COLOR
+            return itemQualityColor:WrapTextInColorCode(itemName), itemTexture, FormatPercentage(quantity / reqQuantity)
+        end
+    end
+end
--- Display/PvPSummary.lua
+++ Display/PvPSummary.lua
@@ -5,6 +5,13 @@ local PvP = addon.PvP
 local PVP_SUMMARY_LABEL = "PvP Progress:"
 local RATED_PVP_LABEL = "Rated PvP"
 
+local itemTextureSettings = {
+    width = 20,
+    height = 20,
+    verticalOffset = 3,
+    margin = { right = 5, bottom = 5 },
+}
+
 Display:RegisterHookScript(LFDMicroButton, "OnEnter", function()
     Display:AddEmptyLine()
     Display:AddHighlightLine(PVP_SUMMARY_LABEL)
@@ -20,8 +27,20 @@ Display:RegisterHookScript(LFDMicroButton, "OnEnter", function()
         Display:AddEmptyLine()
         Display:AddHighlightDoubleLine(RATED_PVP_LABEL, ARENA_WEEKLY_STATS)
 
-        for ratingString, weeklyStatusString in PvP:IterableArenaProgressInfo() do
+        for ratingString, weeklyStatusString, tierName, tierIcon, nextTierName in PvP:IterableArenaProgressInfo() do
             Display:AddRightHighlightDoubleLine(ratingString, weeklyStatusString)
+            if tierName and IsShiftKeyDown() then
+                Display:AddDoubleLine(tierName, nextTierName)
+                Display:AddTexture(tierIcon, itemTextureSettings)
+            end
+        end
+
+        local itemName, itemTexture, progress = PvP:GetSeasonRewardInfo()
+
+        if itemName then
+            Display:AddEmptyLine()
+            Display:AddRightHighlightDoubleLine(itemName, progress)
+            Display:AddTexture(itemTexture, itemTextureSettings)
         end
     elseif isPreseason then
         Display:AddLine(PLAYER_V_PLAYER_PRE_SEASON)
