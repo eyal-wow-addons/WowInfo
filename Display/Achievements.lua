a8c1f2cbe360b672bb8dfc71e05509154698d0dd
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-05-02 22:57:14 +0300

    Fix Achievements tooltip that sometimes wouldn't show up

--- Display/Achievements.lua
+++ Display/Achievements.lua
@@ -3,7 +3,10 @@ local L = addon.L
 local Display = addon:NewDisplay("Achievements")
 local Achievements = addon.Achievements
 
-Display:RegisterHookScript(AchievementMicroButton, "OnEnter", function()
+Display:RegisterHookScript(AchievementMicroButton, "OnEnter", function(self)
+    -- NOTE: Makes sure 'tooltipText' is never nil, otherwise, the tooltip wouldn't show up.
+    self.tooltipText = MicroButtonTooltipText(ACHIEVEMENT_BUTTON, "TOGGLEACHIEVEMENT")
+
     Display:AddTitleLine(L["Summary:"])
     Display:AddRightHighlightDoubleLine(Achievements:GetSummaryProgressString())
     Display:AddEmptyLine()
