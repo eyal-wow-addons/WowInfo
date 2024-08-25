9e128ba18716fa865e29624fe29f05e7e3cc973d
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-08-25 22:45:31 +0300

    Update NewDisplay so tooltip apis are no longer accessed directly

--- Main.lua
+++ Main.lua
@@ -2,6 +2,6 @@ local addon = LibStub("Addon-1.0"):New(...)
 local Tooltip = LibStub("Tooltip-1.0")
 
 function addon:NewDisplay(name)
-    local obj = addon:NewObject(name .. "Display")
-    return Tooltip:CreateProxy(Tooltip, obj)
+    local display = addon:NewObject(name .. "Display")
+    return display, Tooltip
 end
