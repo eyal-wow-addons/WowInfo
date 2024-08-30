8e28b563472ce0cc90bc7fdb55182c2130aa4859
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-08-30 10:49:30 +0300

    Update Main
    1. Add AceDB lib.
    2. Update NewDisplay now returns a proxy object that can access the Tooltip lib and Colonizer.

--- Main.lua
+++ Main.lua
@@ -1,7 +1,22 @@
 local addon = LibStub("Addon-1.0"):New(...)
-local Tooltip = LibStub("Tooltip-1.0")
 
-function addon:NewDisplay(name)
-    local display = addon:NewObject(name .. "Display")
-    return Tooltip:CreateProxy(display)
+function addon:OnInitialized()
+    self.DB = LibStub("AceDB-3.0"):New("WowInfoDB", {}, true)
 end
+
+do
+    local Tooltip = LibStub("Tooltip-1.0")
+    local Colorizer = addon.Colorizer
+
+    local MT = {
+        __index = function(_, key)
+            return Tooltip[key] or Colorizer[key]
+        end
+    }
+
+    function addon:NewDisplay(name)
+        local display = addon:NewObject(name .. "Display")
+        return setmetatable(display, MT)
+    end
+end
+
