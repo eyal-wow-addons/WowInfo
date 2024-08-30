406616be146960f09659762485128889895f1a7b
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-08-30 11:04:43 +0300

    Update Collections now uses the new Display APIs

--- Display/Collections.lua
+++ Display/Collections.lua
@@ -9,27 +9,31 @@ local COLLECTIONS_LABEL_FORMAT = "- %s: |cffffffff%d|r / |cff20ff20%d|r"
 function Display:AddAchievementLine(callback)
     local name, currAmount, reqAmount = callback()
     if name then
-        self:AddLine(COLLECTIONS_LABEL_FORMAT:format(name, currAmount, reqAmount))
+        self:AddFormattedLine(COLLECTIONS_LABEL_FORMAT, name, currAmount, reqAmount)
     end
+    return self
 end
 
 Display:RegisterHookScript(CollectionsMicroButton, "OnEnter", function()
     local totalMounts = Collections:GetTotalMounts()
     if totalMounts  then
-        Display:AddHeader(L["Mounts: X"]:format(totalMounts))
-        Display:AddAchievementLine(Achievements.GetMountAchievementInfo)
+        Display
+            :AddFormattedHeader(L["Mounts: X"], totalMounts)
+            :AddAchievementLine(Achievements.GetMountAchievementInfo)
     end
 
     local totalPets = Collections:GetTotalPets()
     if totalPets then
-        Display:AddHeader(L["Pets: X"]:format(totalPets))
-        Display:AddAchievementLine(Achievements.GetPetsAchievementInfo)
+        Display
+            :AddFormattedHeader(L["Pets: X"], totalPets)
+            :AddAchievementLine(Achievements.GetPetsAchievementInfo)
     end
 
     local totalLearnedToys, totalToys = Collections:GetTotalToys()
     if totalLearnedToys then
-        Display:AddHeader(L["Toys: X / Y"]:format(totalLearnedToys, totalToys))
-        Display:AddAchievementLine(Achievements.GetToysAchievementInfo)
+        Display
+            :AddFormattedHeader(L["Toys: X / Y"], totalLearnedToys, totalToys)
+            :AddAchievementLine(Achievements.GetToysAchievementInfo)
     end
 
     Display:Show()
