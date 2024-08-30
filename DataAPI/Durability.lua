146475bcf5d929a20601f17950296bb9f552c1bf
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-08-30 23:03:45 +0300

    Update Durability

--- DataAPI/Durability.lua
+++ DataAPI/Durability.lua
@@ -1,5 +1,4 @@
 local _, addon = ...
-local L = addon.L
 local Durability = addon:NewObject("Durability")
 
 local NUM_BAG_FRAMES = 4
@@ -31,57 +30,39 @@ local function GetPercentage(current, total)
     end
 end
 
-Durability:RegisterEvents(
-    "UPDATE_INVENTORY_DURABILITY",
-    "PLAYER_EQUIPMENT_CHANGED", function()
-        local inventory, inventoryMax = 0, 0
-        local bags, bagsMax = 0, 0
+local function UpdateDurability()
+    local inventory, inventoryMax = 0, 0
+    local bags, bagsMax = 0, 0
 
-        for _, slot in ipairs(SLOTS) do
-            local durability, durabilityMax = GetInventoryItemDurability(GetInventorySlotInfo(slot))
-            if durability and durabilityMax then
-                local _, exactPct = GetPercentage(durability, durabilityMax)
-                -- Adds item durability values to total inventory durability value
-                inventory = inventory + exactPct
-                inventoryMax = inventoryMax + 1
-            end
+    for _, slot in ipairs(SLOTS) do
+        local durability, durabilityMax = GetInventoryItemDurability(GetInventorySlotInfo(slot))
+        if durability and durabilityMax then
+            local _, exactPct = GetPercentage(durability, durabilityMax)
+            -- Adds item durability values to total inventory durability value
+            inventory = inventory + exactPct
+            inventoryMax = inventoryMax + 1
         end
-
-        -- Scans bags items durability status
-        for bag = 0, NUM_BAG_FRAMES do
-            for slot = 1, C_Container.GetContainerNumSlots(bag) do
-                local durability, durabilityMax = C_Container.GetContainerItemDurability(bag, slot)
-                if durability then
-                    bags = bags + durability
-                    bagsMax = bagsMax + durabilityMax
-                end
-            end
-        end
-
-        inventoryPct = GetPercentage(inventory, inventoryMax)
-        bagsPct = GetPercentage(bags, bagsMax)
-    end)
-
-local function GetColoredText(percent)
-    local r, g
-
-    if percent >= 0.5 then
-        r = (1 - percent) * 2
-        g = 1
-    else
-        r = 1
-        g = percent * 2
     end
 
-    return format("|cff%02x%02x%02x%d%%|r", r * 255, g * 255, 0, (percent * 100))
+    -- Scans bags items durability status
+    for bag = 0, NUM_BAG_FRAMES do
+        for slot = 1, C_Container.GetContainerNumSlots(bag) do
+            local durability, durabilityMax = C_Container.GetContainerItemDurability(bag, slot)
+            if durability then
+                bags = bags + durability
+                bagsMax = bagsMax + durabilityMax
+            end
+        end
+    end
+
+    inventoryPct = GetPercentage(inventory, inventoryMax)
+    bagsPct = GetPercentage(bags, bagsMax)
 end
 
-function Durability:GetInventoryPercentageString()
-    return inventoryPct and GetColoredText(inventoryPct) or L["N/A"]
+Durability:RegisterEvents(
+    "UPDATE_INVENTORY_DURABILITY",
+    "PLAYER_EQUIPMENT_CHANGED", UpdateDurability)
+
+function Durability:GetPercentages()
+    return inventoryPct, bagsPct
 end
-
-function Durability:GetBagsPercentageString()
-    return bagsPct and GetColoredText(bagsPct) or L["None"]
-end
-
-
--- Display/Durability.lua
+++ Display/Durability.lua
@@ -1,11 +1,31 @@
 local _, addon = ...
-local L = addon.L
+local Durability = addon:GetObject("Durability")
 local Display = addon:NewDisplay("Durability")
-local Durability = addon.Durability
+
+local L = addon.L
+
+local function GetColoredString(percent)
+    local r, g
+
+    if percent >= 0.5 then
+        r = (1 - percent) * 2
+        g = 1
+    else
+        r = 1
+        g = percent * 2
+    end
+
+    return ("|cff%02x%02x%02x%d%%|r"):format(r * 255, g * 255, 0, (percent * 100))
+end
 
 Display:RegisterHookScript(CharacterMicroButton, "OnEnter", function()
-    Display:AddTitleLine(L["Durability:"])
-    Display:AddDoubleLine(L["Equipped"], Durability:GetInventoryPercentageString())
-    Display:AddDoubleLine(L["Bags"], Durability:GetBagsPercentageString())
+    local inventoryPct, bagsPct = Durability:GetPercentages()
+
+    inventoryPct = inventoryPct and GetColoredString(inventoryPct) or L["N/A"]
+    bagsPct = bagsPct and GetColoredString(bagsPct) or L["None"]
+
+    Display:AddHeader(L["Durability:"])
+    Display:AddDoubleLine(L["Equipped"], inventoryPct)
+    Display:AddDoubleLine(L["Bags"], bagsPct)
     Display:Show()
 end)
