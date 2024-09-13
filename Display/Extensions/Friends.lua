4ae5b7bfaa9c9fd8f04d10fc5c7685601bb7aa94
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-09-14 01:25:38 +0300

    Update Friends extension
    Extensions are now a first-class concept in the addon and they can access the Tooltip and Colorizer through the extension object itself.

--- Display/Extensions/Friends.lua
+++ Display/Extensions/Friends.lua
@@ -1,7 +1,5 @@
 local _, addon = ...
-local Friends = addon:GetObject("Friends")
-
-local Colorizer = addon.Colorizer
+local Friends = addon:NewExtension("Friends")
 
 local WOW_CHAR_FORMAT = "%s |cffffffff%d|r"
 local WOW_CHAR_GROUPED_FORMAT = "|cffaaaaaa[|r%s|cffaaaaaa]|r"
@@ -9,8 +7,8 @@ local GAME_STATUS_TABLE = { FRIENDS_TEXTURE_OFFLINE, FRIENDS_TEXTURE_ONLINE, FRI
 local GAME_STATUS_FORMAT = "|T%s:0|t %s"
 
 function Friends:GetFormattedCharName(info)
-    local classFileName = info.className and addon.CLASS_NAMES[info.className] or info.classFileName
-    local charName = Colorizer:ToClassColor(classFileName, info.characterName)
+    local classFilename = info.className and addon.CLASS_NAMES[info.className] or info.classFilename
+    local charName = Friends:ToClassColor(classFilename, info.characterName)
     charName = info.characterLevel and WOW_CHAR_FORMAT:format(charName, info.characterLevel)
     charName = info.grouped and WOW_CHAR_GROUPED_FORMAT:format(charName) or charName
     return charName
