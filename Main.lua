cf191d1c75464232296bd66e6044f837fc6c5be4
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-09-14 01:08:31 +0300

    Add Extensions support
    Extensions are objects that reside between the DataAPI and the Display. Their purpose is to provide reusable and extended functionality across multiple Displays.

--- Main.lua
+++ Main.lua
@@ -1,22 +1,49 @@
 local addon = LibStub("Addon-1.0"):New(...)
 
+local Tooltip = LibStub("Tooltip-1.0")
+local Colorizer = addon.Colorizer
+
 function addon:OnInitialized()
     self.DB = LibStub("AceDB-3.0"):New("WowInfoDB", {}, true)
 end
 
 do
-    local Tooltip = LibStub("Tooltip-1.0")
-    local Colorizer = addon.Colorizer
-
     local MT = {
-        __index = function(_, key)
-            return Tooltip[key] or Colorizer[key]
+        __index = function(t, key)
+            return Tooltip[key]
+                    or Colorizer[key]
+                    or rawget(t, "__friend") and t.__friend[key] 
+                    or rawget(t, "__extension") and t.__extension[key]
         end
     }
 
-    function addon:NewDisplay(name)
+    function addon:NewDisplay(name, extensionName)
         local display = addon:NewObject(name .. "Display")
+        local friend = addon:GetObject(name .. "Extension", true)
+        -- When the Display and the Extension have the same name they are considered friends,
+        -- meaning, the Display can access the Extension as if it was part of the same object.
+        display.__friend = friend
+        -- REVIEW: For now each Display can have a single extension.
+        if extensionName then
+            display.__extension = addon:GetObject(extensionName .. "Extension")
+        end
         return setmetatable(display, MT)
     end
 end
 
+do
+    local MT = {
+        __index = function(t, key)
+            return Tooltip[key]
+                    or Colorizer[key]
+                    or rawget(t, "__root") and t.__root[key]
+        end
+    }
+
+    function addon:NewExtension(name)
+        local root = addon:GetObject(name)
+        local extension = addon:NewObject(name .. "Extension")
+        extension.__root = root
+        return setmetatable(extension, MT)
+    end
+end
