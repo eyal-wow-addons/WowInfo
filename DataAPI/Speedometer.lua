18ca7e61f1718dd483feac882d6b0d52be9ed6b0
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-10-03 23:54:28 +0300

    Revamp Speedometer

--- DataAPI/Speedometer.lua
+++ DataAPI/Speedometer.lua
@@ -1,91 +1,49 @@
 local _, addon = ...
-local L = addon.L
-local Timer = addon.Timer
 local Speedometer = addon:NewObject("Speedometer")
 
-local IsPlayerMoving = IsPlayerMoving
-local GetUnitSpeed = GetUnitSpeed
-local GetGlidingInfo = C_PlayerInfo.GetGlidingInfo
-
-local currentUnit = "player"
-local timer
-
-local function FormatSpeed(currentSpeed, status)
-    local speedPercentage = Round(currentSpeed / BASE_MOVEMENT_SPEED * 100)
-    if status and currentSpeed == 0 then
-        return status
-    elseif status then
-        return format(L["S at X%"], status, speedPercentage)
+local function GetSpeedPercent(speed)
+    if speed == 0 then
+        return 0
     else
-        return format("%d%%", speedPercentage)
+        return Round(speed / BASE_MOVEMENT_SPEED * 100)
     end
 end
 
-local function StartedMoving()
-    Speedometer:TriggerEvent("SPEEDOMETER_PLAYER_STARTED_MOVING", Speedometer:GetFormattedCurrentSpeed())
-end
-
-local function StoppedMoving()
-    Speedometer:TriggerEvent("SPEEDOMETER_PLAYER_STOPPED_MOVING", L["Standing"])
-end
-
-function Speedometer:OnBeforeConfig()
-    timer = Timer:Create(function()
-        if IsPlayerMoving() then
-            StartedMoving()
-        else
-            StoppedMoving()
-        end
-    end)
-    timer:Start(0.5)
-end
-
-Speedometer:RegisterEvent("PLAYER_STARTED_MOVING", StartedMoving)
-Speedometer:RegisterEvent("PLAYER_STOPPED_MOVING", StoppedMoving)
-
-Speedometer:RegisterEvent("PLAYER_ENTERING_WORLD", function()
-    if UnitInVehicle("player") then
-        currentUnit = "vehicle"
-    end
-end)
-
-Speedometer:RegisterEvent("UNIT_ENTERED_VEHICLE", function(_, _, unit)
-    if unit == "player" then
-        currentUnit = "vehicle"
-    end
-end)
-
-Speedometer:RegisterEvent("UNIT_EXITED_VEHICLE", function(_, _, unit)
-    if unit == "player" then
-        currentUnit = "player"
-    end
-end)
-
-function Speedometer:GetFormattedPlayerSpeedInfo()
-    local _, runSpeed, flightSpeed, swimSpeed = GetUnitSpeed("player")
-    return FormatSpeed(runSpeed), FormatSpeed(flightSpeed), FormatSpeed(swimSpeed)
-end
-
-function Speedometer:GetCurrentSpeed()
-    local isGliding, _, forwardSpeed = GetGlidingInfo()
-    return isGliding and forwardSpeed or GetUnitSpeed(currentUnit), isGliding
-end
-
-function Speedometer:GetFormattedCurrentSpeed()
+local function GetCurrentSpeedInfo()
     local status
-    local currentSpeed, isGliding = self:GetCurrentSpeed()
+    local currentSpeed = GetUnitSpeed("player")
+    local isGliding, _, forwardSpeed = C_PlayerInfo.GetGlidingInfo()
 
     if isGliding then
-        status = L["Glide"] 
+        status = "Glide"
+        currentSpeed = forwardSpeed
+    elseif currentSpeed == 0 then
+        status = "Stand"
     elseif IsFlying() then
-        status = L["Fly"]
+        status = "Fly"
     elseif IsSwimming() then
-        status = L["Swim"]
-    elseif IsPlayerMoving() and currentSpeed == 0 then
-        status = L["Moving"]
-    elseif IsPlayerMoving() then
-        status = L["Move"]
+        status = "Swim"
+    elseif UnitOnTaxi("player") then
+        status = "Taxi"
+    elseif IsMounted() then
+        status = "Ride"
+    else
+        status = "Move"
     end
 
-    return FormatSpeed(currentSpeed, status)
+    return status, GetSpeedPercent(currentSpeed)
 end
+
+function Speedometer:OnInitialized()
+    C_Timer.NewTicker(0.5, function()
+        local status, currentSpeed = GetCurrentSpeedInfo()
+        if status == "Stand" then
+            Speedometer:TriggerEvent("WOWINFO_PLAYER_STOPPED_MOVING")
+        else
+            currentSpeed = format("%d%%", currentSpeed)
+            Speedometer:TriggerEvent("WOWINFO_PLAYER_STARTED_MOVING", status, currentSpeed)
+        end
+    end)
+end
+
+
--- Display/Speedometer.lua
+++ Display/Speedometer.lua
@@ -1,37 +1,40 @@
 local _, addon = ...
-local L = addon.L
+local Speedometer = addon:GetObject("Speedometer")
+
 local LDB = LibStub("LibDataBroker-1.1")
-local Speedometer = addon.Speedometer
+
+local L = addon.L
 
 local dataobj
 
-local function UpdateText()
+local function UpdateTextWhenStartedMoving(_, _, status, currentSpeed)
+    status = L["S : S"]:format(currentSpeed, L[status])
     if MinimapCluster.ZoneTextButton:IsVisible() then
+        if MinimapCluster:IsEventRegistered("ZONE_CHANGED") then
+            MinimapCluster:UnregisterEvent("ZONE_CHANGED")
+            MinimapCluster:UnregisterEvent("ZONE_CHANGED_INDOORS")
+            MinimapCluster:UnregisterEvent("ZONE_CHANGED_NEW_AREA")
+        end
         MinimapZoneText:SetTextColor(NORMAL_FONT_COLOR:GetRGB())
-        MinimapZoneText:SetText(Speedometer:GetFormattedCurrentSpeed())
-    end
-end
-
-local function UpdateTextWhenStartedMoving(_, _, status)
-    if MinimapCluster.ZoneTextButton:IsVisible() then
-        MinimapCluster.ZoneTextButton:SetScript("OnUpdate", UpdateText)
+        MinimapZoneText:SetText(status)
     end
     dataobj.text = status
 end
 
-local function UpdateTextWhenStoppedMoving(_, _, status)
+local function UpdateTextWhenStoppedMoving()
     if MinimapCluster.ZoneTextButton:IsVisible() then
-        MinimapCluster.ZoneTextButton:SetScript("OnUpdate", nil)
+        if not MinimapCluster:IsEventRegistered("ZONE_CHANGED") then
+            MinimapCluster:RegisterEvent("ZONE_CHANGED")
+            MinimapCluster:RegisterEvent("ZONE_CHANGED_INDOORS")
+            MinimapCluster:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+        end
         Minimap_Update()
     end
-    if UnitOnTaxi("player") then
-        status = L["Taxi"]
-    end
-    dataobj.text = status
+    dataobj.text = ""
 end
 
-Speedometer:RegisterEvent("SPEEDOMETER_PLAYER_STARTED_MOVING", UpdateTextWhenStartedMoving)
-Speedometer:RegisterEvent("SPEEDOMETER_PLAYER_STOPPED_MOVING", UpdateTextWhenStoppedMoving)
+Speedometer:RegisterEvent("WOWINFO_PLAYER_STARTED_MOVING", UpdateTextWhenStartedMoving)
+Speedometer:RegisterEvent("WOWINFO_PLAYER_STOPPED_MOVING", UpdateTextWhenStoppedMoving)
 
 dataobj = LDB:NewDataObject("Speedometer", {
     type = "data source"
