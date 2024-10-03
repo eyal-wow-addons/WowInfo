09f9a6e463889b9bc0f2acb662b9a5f39327ae19
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-10-03 14:38:19 +0300

    Revamp Talents
    1. Loadouts data is now cached.
    2. Update the Display.

--- DataAPI/Talents.lua
+++ DataAPI/Talents.lua
@@ -1,67 +1,129 @@
 local _, addon = ...
 local Talents = addon:NewObject("Talents")
 
-function Talents:GetSpecString()
+local INFO = {
+    TRAIT = {}
+}
+
+local CACHE = {
+    TRAITS = {}
+}
+
+local function CacheLoadoutsInfo()
+    local specID = specID or PlayerUtil.GetCurrentSpecID()
+    local configIDs = C_ClassTalents.GetConfigIDsBySpecID(specID)
+    local size = math.max(#configIDs, #CACHE.TRAITS)
+    for i = 1, size do
+        local configID = configIDs[i]
+        local data = CACHE.TRAITS[i]
+        if configID then
+            local isActive = false
+            local configInfo = C_Traits.GetConfigInfo(configID)
+            
+            if not data then
+                CACHE.TRAITS[i] = {}
+                data = CACHE.TRAITS[i]
+            end
+
+            data.name = configInfo.name
+            data.usesSharedActionBars = configInfo.usesSharedActionBars
+            data.configID = configID
+            data.specID = specID
+        else
+            CACHE.TRAITS[i] = nil
+        end
+    end
+end
+
+Talents:RegisterEvents(
+    "PLAYER_LOGIN",
+    "ACTIVE_PLAYER_SPECIALIZATION_CHANGED",
+    "SELECTED_LOADOUT_CHANGED", 
+    "TRAIT_CONFIG_CREATED",
+    "TRAIT_CONFIG_DELETED",
+    "TRAIT_CONFIG_UPDATED",
+    "TRAIT_CONFIG_LIST_UPDATED", CacheLoadoutsInfo)
+
+function Talents:GetCurrentSpec()
     local _, classFilename = UnitClass("player")
-    local specString
+    local specName
     local currentSpecID = PlayerUtil.GetCurrentSpecID()
 
     for i = 1, GetNumSpecializations() do
         local id, name = GetSpecializationInfo(i)
         if id == currentSpecID then
-            local classColor = RAID_CLASS_COLORS[classFilename]
-            specString = WrapTextInColor(name, classColor)
+            specName = name
             break
         end
     end
 
-    return specString
+    return specName
+end
+
+function Talents:HasLoadouts()
+    return #CACHE.TRAITS > 0 and true or false
 end
 
 function Talents:IterableLoadoutsInfo()
+    INFO.TRAIT.name = nil
+    INFO.TRAIT.usesSharedActionBars = nil
+    INFO.TRAIT.isActive = nil
+
+    local isStarterBuildActive = self:IsStarterBuildActive()
     local i = 0
-    local specID = specID or PlayerUtil.GetCurrentSpecID()
-    local configIDs = C_ClassTalents.GetConfigIDsBySpecID(specID)
-    local n = configIDs and #configIDs or 0
-    if n > 0 then
-        self:TriggerEvent("TALENTS_SHOW_LOADOUTS")
-    end
+    local n = #CACHE.TRAITS
+
     return function()
         i = i + 1
         if i <= n then
-            local isActive = false
-            local configID = configIDs[i]
-            local configInfo = C_Traits.GetConfigInfo(configID)
-            local lastSelectedSavedConfigID = C_ClassTalents.GetLastSelectedSavedConfigID(specID)
-            if configID == lastSelectedSavedConfigID then
-                isActive = true
+            local data = CACHE.TRAITS[i]
+
+            INFO.TRAIT.name = data.name
+            INFO.TRAIT.usesSharedActionBars = data.usesSharedActionBars
+            INFO.TRAIT.isActive = false
+
+            local lastSelectedSavedConfigID = C_ClassTalents.GetLastSelectedSavedConfigID(data.specID)
+
+            if not isStarterBuildActive and data.configID == lastSelectedSavedConfigID then
+                INFO.TRAIT.isActive = true
             end
-            return configInfo, isActive
+            
+            return INFO.TRAIT
         end
     end
 end
 
-function Talents:GetStarterBuildInfo()
+function Talents:IsStarterBuildActive()
     local hasStarterBuild = C_ClassTalents.GetHasStarterBuild()
     if hasStarterBuild then
-        self:TriggerEvent("TALENTS_SHOW_LOADOUTS")
-        return TALENT_FRAME_DROP_DOWN_STARTER_BUILD, C_ClassTalents.GetStarterBuildActive()
+        return C_ClassTalents.GetStarterBuildActive()
     end
 end
 
-function Talents:IteratablePvPTalents()
+function Talents:HasPvpTalents()
+    for name in self:IteratablePvpTalents() do
+        if name then
+            return true
+        end
+    end
+    return false
+end
+
+function Talents:IteratablePvpTalents()
     local i = 0
     local t = C_SpecializationInfo.GetAllSelectedPvpTalentIDs()
     local n = #t
-    if n > 0 then
-        self:TriggerEvent("TALENTS_SHOW_PVP_TALENTS")
-    end
     return function()
         i = i + 1
-        if i <= n then
+        while i <= n do
             local talentID = t[i]
-            local _, name, icon, _, _, _, unlocked = GetPvpTalentInfoByID(talentID)
-            return name, icon, unlocked
+            if talentID then
+                local _, name, icon, _, _, _, unlocked = GetPvpTalentInfoByID(talentID)
+                if name then
+                    return name, icon, unlocked
+                end
+            end
+            i = i + 1
         end
     end
 end
--- Display/Talents.lua
+++ Display/Talents.lua
@@ -1,54 +1,57 @@
 local _, addon = ...
-local L = addon.L
+local Talents = addon:GetObject("Talents")
 local Display = addon:NewDisplay("Talents")
-local Talents = addon.Talents
+
+local L = addon.L
 
 local TALENTS_LOADOUT_SHARED_ACTION_BARS = "%s*"
-
-Talents:RegisterEvent("TALENTS_SHOW_LOADOUTS", function()
-    Display:AddTitleLine(L["Loadouts:"], true)
-end)
-
-Talents:RegisterEvent("TALENTS_SHOW_PVP_TALENTS", function()
-    Display:AddTitleLine(L["PvP Talents:"], true)
-end)
 
 Display:RegisterHookScript(PlayerSpellsMicroButton, "OnEnter", function(self)
     if not self:IsEnabled() then
         return
     end
 
-    local spec = Talents:GetSpecString()
+    local spec = Talents:GetCurrentSpec()
 
     if spec then
-        Display:AddTitleLine(L["Specialization: S"]:format(spec))
+        Display:AddFormattedHeader(L["Specialization: S"], Display:ToPlayerClassColor(spec))
     end
 
-    local starterBuildName, isStarterBuildActive = Talents:GetStarterBuildInfo()
+    if Talents:HasLoadouts() then
+        Display:AddHeader(L["Loadouts:"])
 
-    if starterBuildName then
-        if isStarterBuildActive then
-            Display:AddGreenLine(starterBuildName)
+        Display:SetLine(TALENT_FRAME_DROP_DOWN_STARTER_BUILD)
+        if Talents:IsStarterBuildActive() then
+            Display:SetGreenColor()
         else
-            Display:AddGrayLine(starterBuildName)
+            Display:SetGrayColor()
+        end
+        Display:ToLine()
+
+        for info in Talents:IterableLoadoutsInfo() do
+            if info and info.name then
+                local name = info.name
+                if info.usesSharedActionBars then
+                    name = TALENTS_LOADOUT_SHARED_ACTION_BARS:format(name)
+                end
+                Display:SetLine(name)
+                if info.isActive then
+                    Display:SetGreenColor()
+                else
+                    Display:SetGrayColor()
+                end
+                Display:ToLine()
+            end
         end
     end
 
-    for configInfo, isActive in Talents:IterableLoadoutsInfo() do
-        local name = configInfo.name
-        if configInfo.usesSharedActionBars then
-            name = TALENTS_LOADOUT_SHARED_ACTION_BARS:format(name)
-        end
-        if isActive then
-            Display:AddGreenLine(name)
-        else
-            Display:AddGrayLine(name)
-        end
-    end
+    if Talents:HasPvpTalents() then
+        Display:AddHeader(L["PvP Talents:"])
 
-    for name, icon in Talents:IteratablePvPTalents() do
-        Display:AddLine(name)
-        Display:AddIcon(icon)
+        for name, icon in Talents:IteratablePvpTalents() do
+            Display:AddLine(name)
+            Display:AddIcon(icon)
+        end
     end
 
     Display:Show()
