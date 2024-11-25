dedc45a4571ecb2560f9a601428ab9bd48367f54
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-11-25 13:20:20 +0200

    Add API to get the default value

--- Storage/Guild.lua
+++ Storage/Guild.lua
@@ -11,6 +11,21 @@ function Storage:OnInitialized()
     DB = self:RegisterDB(defaults)
 end
 
+local function GetDefault(key, tbl)
+    for k, v in pairs() do
+		if type(v) == "table" then
+			return GetDefault(key, v)
+        elseif k == key then
+            return v
+		end
+	end
+    -- TODO: throw if not found ynice?
+end
+
+function Storage:GetDefault(key)
+    return GetDefault(key, defaults)
+end
+
 function Storage:GetMaxOnlineFriends()
     return DB.profile.maxOnlineFriends
 end
